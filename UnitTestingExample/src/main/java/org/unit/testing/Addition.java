a030d555f7885aa4d2d754c19f80c61af724bada
Md Imran Alam <mdimranalam2348@gmail.com>
2024-05-29 22:01:39 +0530

    Update Addition.java

--- UnitTestingExample/src/main/java/org/unit/testing/Addition.java
+++ UnitTestingExample/src/main/java/org/unit/testing/Addition.java
@@ -5,7 +5,7 @@ public class Addition {
     public String add(String s1,String s2){
         return s1+s2;
     }
-
+    
     // create a method for add two integer
     public static int add(int a, int b){
         return a+b;
