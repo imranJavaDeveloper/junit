a9ced786487ffb3ef2920617a0ad9bb272a491af
Md Imran Alam <mdimranalam2348@gmail.com>
2024-05-29 22:03:01 +0530

    some changes done in unit testing file

--- UnitTestingExample/src/main/java/org/unit/testing/Addition.java
+++ UnitTestingExample/src/main/java/org/unit/testing/Addition.java
@@ -5,12 +5,12 @@ public class Addition {
     public String add(String s1,String s2){
         return s1+s2;
     }
-    
+
     // create a method for add two integer
     public static int add(int a, int b){
         return a+b;
     }
-
+    
     // create a method for add two  float value
     public float add(float a,float b){
         return a+b;
