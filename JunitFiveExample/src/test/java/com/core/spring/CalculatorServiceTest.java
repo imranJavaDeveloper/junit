663da73f2df91d0cc73259ef0ff748bff66ef667
Md Imran Alam <mdimranalam2348@gmail.com>
2024-08-11 23:45:09 +0530

    Update CalculatorServiceTest.java

--- JunitFiveExample/src/test/java/com/core/spring/CalculatorServiceTest.java
+++ JunitFiveExample/src/test/java/com/core/spring/CalculatorServiceTest.java
@@ -1,14 +1,14 @@
 package com.core.spring;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorServiceTest {
-
+/*  Test the praticular bloack of the code indepedet*/
     @Test
     public void add(){
         int result=CalculatorService.add(25,32);
-
         int expected=57;
         Assertions.assertEquals(expected,result);
         System.out.println("add method called");
@@ -19,12 +19,9 @@ public class CalculatorServiceTest {
     public void divide(){
         int result=CalculatorService.divide(45,9);
         int expected=5;
-
         Assertions.assertEquals(result,expected);
         System.out.println("divide method called");
     }
-
-
     @Test
     public void addTwoString(){
         String result=CalculatorService.addTwoString("md ","imran alam");
@@ -33,8 +30,8 @@ public class CalculatorServiceTest {
         Assertions.assertEquals(result,expected);
         System.out.println("addTwoString method called");
     }
-
-    @Before
+  /* this test case run before all the test */
+    @BeforeAll
     public void cleanUp(){
         System.out.println(" preform for clean up Activity");
     }
