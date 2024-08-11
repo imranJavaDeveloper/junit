b8df3b33d4135161c1134f5255ed84fbf69d43c3
Md Imran Alam <mdimranalam2348@gmail.com>
2024-08-11 23:42:11 +0530

    Update CalculatorService.java

--- JunitFiveExample/src/main/java/com/core/spring/CalculatorService.java
+++ JunitFiveExample/src/main/java/com/core/spring/CalculatorService.java
@@ -1,24 +1,21 @@
 package com.core.spring;
 public class CalculatorService {
 
-    // create add method
+    /*create add method */
 
     public static int add(int first,int second){
-
         int sum=first+second;
         return sum;
     }
 
-    // create divide method
+    /* create divide method*/ 
     public static int divide(int first,int second){
-
         int div=first/second;
         return div;
     }
 
-    // Add two String
+    /*  Add two String */
     public static String addTwoString(String s1,String s2){
-
         return s1+s2;
     }
 
